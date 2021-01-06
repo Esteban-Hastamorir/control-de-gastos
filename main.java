307435d7fdbd48bae3cc561e1c1f6d0f85112e7f
edwin esteban hastamorir martinez <edwin.hastamorirm@utadeo.edu.co>
2021-01-06 17:36:25 -0500

    se hicieron correciones a los nombre de los archivos y se avanzo en la logica de main

--- main.java
+++ main.java
@@ -1,8 +1,21 @@
-
+import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
-			
+		Scanner input = new Scanner(System.in);
+			System.out.println("Buen día, este programa tiene como finalidad ayudarte con tus finanzas personales\nDime por favor que tipo de movimiento vas aregistrar\n"
+					+ "[1] Ingreso\n[2] Gasto");
+			int opcion = input.nextInt();
+			if (opcion == 1) {
+				
+			}
+			else if (opcion == 2) {
+				
+			}
+			else {
+				System.out.println("Error, ingres un dato valido");
+				main(args);
+			}
 
 	}
 
